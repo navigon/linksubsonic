e139c969b881da8331d171d8f9e26b57ac356b8a
navigon <johntitorfr@gmail.com>
2014-07-23 14:19:56 +0200

    linksubsonic
    Plugin ruTorrent pour afficher subsonic dans la toolbar

---
+++ init.js
@@ -0,0 +1,19 @@
+/* URL de  Subsonic sur le serveur */
+var url = 'xx.xx.xx.xx:xxxx';
+
+plugin.loadLang(true);
+if (theWebUI.theme) {
+  if (theWebUI.theme == 'Oblivion') {
+    plugin.loadCSS("linksubsonicoblivion");
+  } else {
+    plugin.loadCSS("linksubsonic");
+  }
+} else {
+  plugin.loadCSS("linksubsonic");
+}
+
+plugin.onLangLoaded = function()
+{
+  this.addButtonToToolbar("linksubsonic", theUILang.linksubsonic, "window.open('"+url+"')", "help");
+  this.addSeparatorToToolbar("help");
+}
---
+++ lang/pt.js
@@ -0,0 +1,1 @@
+theUILang.linksubsonic = "Subsonic";
